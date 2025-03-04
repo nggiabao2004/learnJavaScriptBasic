6d9182ae3f2a2a9e467b73968864802a8deeca64
nggiabao2004 <nguyengiabao022004@gmail.com>
2025-03-04 07:34:18 +0700

    first commit

---
+++ chuong2/bai2/main.js
@@ -0,0 +1,8 @@
+alert('Hello World');
+
+var kiTuChu= 'Hello World'
+var kiTuSo= 123;
+//var không cần dấu ";", nhưng khuyên nên có
+
+alert(kiTuChu);
+alert(kiTuSo);
---
+++ chuong2/bai4/main.js
@@ -0,0 +1,26 @@
+//console
+console.log('Day la dong note');
+console.log(123);
+
+var hi= 'hello';
+console.log(hi);
+console.warn(hi);
+console.error(hi);
+
+//confirm
+confirm('Xac nhan ban du tuoi!');
+//prompt: label
+prompt('Xac nhan ban du tuoi');
+
+//setTimeOut
+setTimeout(function() {
+    alert('Thong bao sau 1 giay!')
+}, 1000) //1000 miliSecond= 1 giay
+
+//set interval
+setInterval(function() {
+    alert('Thong bao sau 1 giay!')
+}, 1000) //Thuc thi lien tuc sau 1 giay
+setInterval(function() {
+    console.log('Day la log'+ Math.random())
+}, 1000)
